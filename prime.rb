54d642c3d0df07ae10933c9ca5fdde9d965c509e
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-05-08 01:23:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
-def prime? (x)
-  if x <= 1
+def prime?(num)
+  if num <= 1
     false
-  elsif x <= 3
+  else num == 2
     true
+  else
+    (2..num-1).to_a.all? do |i|
+      num % i != 0
+    end
   end
 end
+  
+    
